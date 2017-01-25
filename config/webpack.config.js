62ea2d84d13195c6187e20cf4d0aa382b6fb6c5d
Wink Saville <wink@saville.com>
2017-01-25 10:32:48 -0800

    Update webpack.config.js
    Being more explicit on the location of various paths and extensions.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,8 +1,22 @@
+var path = require('path');
+var prjPath = path.resolve();
+var srcPath = path.resolve("src");
+var libPath = path.resolve("lib");
+var modulePath = path.resolve("node_modules");
+
 module.exports = {
-  entry: "./src/entry.js",
+  entry: path.join(srcPath, "entry.js"),
   output: {
-    path: "./dist",
+    path: path.join(prjPath, "dist"),
     filename: "bundle.js"
+  },
+  resolve: {
+    modules: [
+      srcPath, libPath, modulePath
+    ],
+    extensions: [
+      ".js", ".json"
+    ]
   },
   module: {
     loaders: [
